5cc108f774050cc9682e9a9792e258400300868c
Davide Sotan <davide.sotan@itiscuneo.eu>
2023-03-10 09:46:14 +0100

    Started handling android permissions on internet, network state and camera

---
+++ AndroidApp/UI/app/src/main/java/com/example/ui/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_create);
+
+        checkAndroidPermissions();
+
+    }
+
+    private void checkAndroidPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            String[] permissions = {Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.CAMERA};
+            int idPermissionRequest = 1;
+            ActivityCompat.requestPermissions(this, permissions, idPermissionRequest);
+        }
+    }
+}
