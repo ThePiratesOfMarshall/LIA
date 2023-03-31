f7d4b7e2643c01699cba2da30a631977da178cdf
alessandromoriondo <alessandro.moriondo@itiscuneo.eu>
2023-03-31 08:03:54 +0200

    Added rdirect to DB

---
+++ WebApp/ParaQr.java
@@ -0,0 +1,27 @@
+public class ParaQr{
+    String codice = null;
+    private String link = null;
+
+    public ParaQr(){
+        this("000000", null);
+    }
+    public ParaQr(String codice, String link){
+        this.codice = codice;
+        this.link = link;
+    }
+
+    public String getCodice(){
+        return this.codice;
+    }
+    public void setCodice(String codice){
+        this.codice = codice;
+    }
+
+    public String getLink(){
+        return this.link;
+    }
+    public void setLink(String link){
+        this.link = link;
+    }
+    
+}
---
+++ WebApp/Reindirizzamento.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Reidirizzamento{
+    public static ArrayList<ParaQr> elenco = new ArrayList<ParaQr>();
+
+    public static void carica(){
+        System.out.println("sto caricando il database");
+        elenco.add(new ParaQr("0", "bbbb"));
+    }
+
+    public static void leggi(String link){
+        ParaQr qr = new ParaQr();
+        qr.setLink(link);
+        System.out.println(link);
+        
+        //scorro tutta la tabella fin quando il codice meccia
+        for(ParaQr indice : elenco){
+            if(qr.codice == indice.getCodice()){
+                System.out.println("corrispondenza trovata");
+                System.out.println(qr.codice);
+                qr.setCodice(qr.codice);
+            }
+        }
+    }
+    public static void main(String[] args){
+        String link = "bbbb";
+
+        carica(); //caricare nella tabella l'elenco dei codici collegati a ogni singolo qr code
+
+        leggi(link);
+    }
+}
