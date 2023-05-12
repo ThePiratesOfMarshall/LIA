0f25cd01dcc9fee3c4ff1d943f67a142af3ae4e4
Davide Sotan <davide.sotan@itiscuneo.eu>
2023-05-12 09:20:16 +0200

    Modified fragment_home layout and started working on qr scanner

--- AndroidApp/UI/app/src/main/java/com/example/ui/Home.java
+++ AndroidApp/UI/app/src/main/java/com/example/ui/Home.java
@@ -1,12 +1,15 @@
 package com.example.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.ui.databinding.FragmentHomeBinding;
+import com.google.zxing.*;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +26,8 @@ public class Home extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FragmentHomeBinding binding;
 
     public Home() {
         // Required empty public constructor
@@ -58,7 +63,29 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    IntentIntegrator scanIntegrator = new IntentIntegrator(context);
+                    scanIntegrator.setPrompt("Scan");
+                    scanIntegrator.setBeepEnabled(true);
+                    //The following line if you want QR code
+                    scanIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                    scanIntegrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+                    scanIntegrator.setOrientationLocked(true);
+                    scanIntegrator.setBarcodeImageEnabled(true);
+                    scanIntegrator.initiateScan();
+
+                }
+            }
+        });
     }
 }
